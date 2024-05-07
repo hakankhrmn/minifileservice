bf1adfa431b0c824a753c9ad7c9a1c4f42be45f0
Hakan Kahraman <hakan.kahraman@layermark.com>
2024-05-08 01:28:07 +0300

    Initialize Project

---
+++ src/main/java/com/cyangate/minifileservice/MinifileserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cyangate.minifileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinifileserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinifileserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyangate/minifileservice/MinifileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyangate.minifileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinifileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
